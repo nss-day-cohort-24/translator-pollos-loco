ef8cefbe7652832054c3f10543e11bc08d7d76b3
Sam Kimball <jibequan@gmail.com>
2018-02-05 23:02:45 -0600

    Added button, linked chinese.js and create translator var, ++js for btn

---
+++ js/chinese.js
@@ -0,0 +1,9 @@
+"use strict";
+
+ // var dictionary = {"happy":"快乐", "presidents'":"总统", "day":"天"}
+
+function chinese(string) {
+  return string;
+}
+
+module.exports = chinese;
--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 "use strict";
 
 console.log("Hello main.js");
+
+let translateToChinese = require("./chinese");
+// Brittany,
+// Hayden,
+// Jeremy
+
+let translator = {
+  translateToChinese
+  // Brittany,
+  // Hayden,
+  // Jeremy
+};
+
+console.log(translator.translateToChinese());
+
+function dropDown() {
+    document.getElementById("whichLang").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
